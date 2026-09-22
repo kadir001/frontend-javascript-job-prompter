a2da62a55c070a3a800585cc25987db14fdacdfd
Kadir Akdemir <kadirakdemir75@gmail.com>
2026-09-22 16:06:05 +0200

    opdracht 1 en 2

--- scripts/main.js
+++ scripts/main.js
@@ -71,4 +71,44 @@ const departments = {
     }
 }
 
+
+
+const userInput = prompt(
+    "Over welke afdeling wil je meer informatie? Kies uit: [marketing / sales / customer-service]"
+);
+
+if (userInput === "marketing") {
+    console.log(`Je koos ${userInput}. ${departments.marketing.description}`);
+} else if (userInput === "sales") {
+    console.log(`Je koos ${userInput}. ${departments.sales.description}`);
+} else if (userInput === "customer-service") {
+    console.log(`Je koos ${userInput}. ${departments["customer-service"].description}`);
+} else {
+    console.error(
+        "Ongeldige keuze. Probeer het opnieuw door de pagina te verversen."
+    );
+}
+
+console.log(userInput);
+
 console.log(departments);
+
+// Opdracht 1a
+console.log(
+    `De afdeling Sales heeft ${departments.sales.numberOfEmployees} medewerkers`
+);
+
+// Opdracht 1b
+console.log(
+    `Marketing is een leuke afdeling om te werken. ${departments.marketing.description}`
+);
+
+// Opdracht 1c
+console.log(
+    `De afdeling Customer Service heeft ${departments["customer-service"].numberOfEmployees} medewerkers`
+);
+
+// Opdracht 1d
+console.log(
+    `Sales is een uitdagende afdeling om te werken als Verkoopmanager. ${departments.sales.jobs[1].description}`
+);
